80ccacd23f2a85a18d1f17b8b8b5ac771d6cba73
Naveed <naveed.jalal@edu.novia.fi>
2020-04-15 16:42:20 +0000

    slots in vue js added

---
+++ SlotsinVue/slots-vue/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+   <Button>
+     <template #icon>
+               <i class="fas fa-shopping-cart"></i>
+     </template>
+      
+      <span @click="test" class="text orange">
+        {{ label }}
+      </span>
+   </Button>
+ 
+  </div>
+</template>
+
+<script>
+
+import Button from "./components/Button";
+export default {
+  data: () => {
+    return {
+      label:'Order Now'
+    }
+  },
+  components: {
+    Button
+  },
+  methods: {
+    test(){
+      console.log('Hello World');
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://use.fontawesome.com/releases/v5.9.0/css/all.css");
+body {
+  display:flex;
+  justify-content:center;
+  align-items:flex-start;
+  width:100%;
+  height:100vh;
+  padding:0;
+  margin:0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 1rem;
+}
+#app{
+  padding:20px;
+  color:cadetblue;
+  background-color:white
+}
+.orange{
+  color:white;
+  background-color:goldenrod;
+}
+</style>
---
+++ SlotsinVue/slots-vue/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>
+        <span class="icon orange">
+           <slot name="icon"> </slot>
+      </span>
+   
+     <slot>
+          <span class="text orange">
+              Submit
+          </span>
+    
+     </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
